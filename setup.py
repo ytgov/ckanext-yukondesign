272fef62d280dc87687cb8c50281dda30f3beddc
gavram <dragan.avramovic@datopian.com>
2025-03-13 11:13:09 +0100

    Fixing setup configuration

--- setup.py
+++ setup.py
@@ -1,25 +1,4 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup
 
-# Note: Do not add new arguments to setup(), instead add setuptools
-# configuration options to setup.cfg, or any other project information
-# to pyproject.toml
-# See https://github.com/ckan/ckan/issues/8382 for details
-
-setup(
-    # If you are changing from the default layout of your extension, you may
-    # have to change the message extractors, you can read more about babel
-    # message extraction at
-    # http://babel.pocoo.org/docs/messages/#extraction-method-mapping-and-configuration
-    message_extractors={
-        'ckanext': [
-            ('**.py', 'python', None),
-            ('**.js', 'javascript', None),
-            ('**/templates/**.html', 'ckan', None),
-        ],
-    },
-    entry_points='''
-        yukon_2025_design = ckanext.yukon_2025_design.plugin:Yukon2025DesignPlugin
-    ''',
-    
-)
+setup()
